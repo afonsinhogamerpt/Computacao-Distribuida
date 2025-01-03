fccc3fb1ddcb88b263dd646c307103438f6d0d6d
Cyb3rWolf945 <59710384+Cyb3rWolf945@users.noreply.github.com>
2025-01-03 15:40:34 +0000

    update for testing login

--- CD/src/cd/client/AuthenticationClient.java
+++ CD/src/cd/client/AuthenticationClient.java
@@ -16,50 +16,52 @@ import shared.User;
 public class AuthenticationClient {
     
     public static void main(String[] args) {
-        try {
-            AuthenticationService authService = (AuthenticationService) Naming.lookup("//localhost:1099/AuthenticationService");
-
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Digite 1 para Login ou 2 para Registro:");
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-
-            System.out.println("Digite o nome de utilizador:");
-            String username = scanner.nextLine();
-            System.out.println("Digite a senha:");
-            String password = scanner.nextLine();
-
-            if (choice == 1) {
-                // Receber o objeto User ao invés de um boolean
-                User user = authService.login(username, password);
-
-                if (user != null) {
-                    System.out.println("Login bem-sucedido!");
-                    System.out.println("utilizador: " + user.getNome());
-                    System.out.println("Tipo de utilizador: " + user.getUserType());
-                } else {
-                    System.out.println("Falha no login.");
-                }
-
-            } else if (choice == 2) {
-                System.out.println("Digite o tipo de utilizador (NORMAL ou INSTITUICAO):");
-                String userType = scanner.nextLine();
-
-                User user = new User(username, userType);
-                user.generateKeys();
-                user.save(password);
-
-                if (authService.register(username, password, userType)) {
-                    System.out.println("Registro bem-sucedido!");
-                } else {
-                    System.out.println("Falha no registro.");
-                }
-            } else {
-                System.out.println("Opção inválida.");
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            AuthenticationService authService = (AuthenticationService) Naming.lookup("//localhost:1099/AuthenticationService");
+//
+//            Scanner scanner = new Scanner(System.in);
+//            System.out.println("Digite 1 para Login ou 2 para Registro:");
+//            int choice = scanner.nextInt();
+//            scanner.nextLine();
+//
+//            System.out.println("Digite o nome de utilizador:");
+//            String username = scanner.nextLine();
+//            System.out.println("Digite a senha:");
+//            String password = scanner.nextLine();
+//
+//            if (choice == 1) {
+//                // Receber o objeto User ao invés de um boolean
+//                User user = authService.login(username, password);
+//
+//                if (user != null) {
+//                    System.out.println("Login bem-sucedido!");
+//                    System.out.println("utilizador: " + user.getNome());
+//                    System.out.println("Tipo de utilizador: " + user.getUserType());
+//                } else {
+//                    System.out.println("Falha no login.");
+//                }
+//
+//            } else if (choice == 2) {
+//                System.out.println("Digite o tipo de utilizador (NORMAL ou INSTITUICAO):");
+//                String userType = scanner.nextLine();
+//
+//                User user = new User(username, userType);
+//                user.generateKeys();
+//                user.save(password);
+//
+//
+//                if (authService.register(username, password, userType)) {
+//                    System.out.println("Registro bem-sucedido!");
+//                } else {
+//                    System.out.println("Falha no registro.");
+//                }
+//            } else {
+//                System.out.println("Opção inválida.");
+//            }
+//
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 }
